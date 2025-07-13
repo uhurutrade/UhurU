6e38ef8bd78e323d236d0f58e8bf227f789572a3
Uhuru Trade <uhurutradeuk@gmail.com>
2025-07-13 12:52:02 +0000

    voz masculina y española la voz por favor

--- src/ai/flows/chat-flow.ts
+++ src/ai/flows/chat-flow.ts
@@ -80,7 +80,7 @@ const ttsFlow = ai.defineFlow(
         responseModalities: ['AUDIO'],
         speechConfig: {
           voiceConfig: {
-            prebuiltVoiceConfig: { voiceName: 'Achernar' },
+            prebuiltVoiceConfig: { voiceName: 'Zubenelgenubi' },
           },
         },
       },
