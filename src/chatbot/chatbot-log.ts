696d5553531eb8e4132ff5f781a73044cdec2420
Uhuru Trade <uhurutradeuk@gmail.com>
2025-07-16 12:37:37 +0000

    mete este fichero en el gitignore chatbot-log.ts

---
+++ src/chatbot/chatbot-log.ts
@@ -0,0 +1,3 @@
+[16-07-2025 11:20:36][id:538888] uhurulog_chat: {"ip":"213.4.127.166","history":[],"isVoiceInput":false,"input_newUserMessage":"sad"}
+[16-07-2025 11:20:38][id:538888] uhurulog_chat: {"ip":"213.4.127.166","status":"calling_ai_generate"}
+[16-07-2025 11:20:41][id:538888] uhurulog_chat: {"ip":"213.4.127.166","output_ai_response":"Lo siento, no entiendo qué quieres decir con \"sad\". ¿Hay algo en lo que pueda ayudarte?\n"}
