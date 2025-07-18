e529553e5235b9a0d9260a113bc06b2623c0ced6
Uhuru Trade <uhurutradeuk@gmail.com>
2025-07-18 17:39:56 +0000

    intenta que pregunte el noombre con la excusa de Hola, en que te puedo a

--- src/chatbot/chatbot-welcome.ts
+++ src/chatbot/chatbot-welcome.ts
@@ -1,3 +1,2 @@
 
-export const chatbotWelcomeMessage = "Hello! I'm UhurU's AI assistant. Whether you need to uplift your technological edge, optimise your tax planning, or uncover new avenues for investment, I'm on hand to boost your business growth and efficiency.";
-
+export const chatbotWelcomeMessage = "Hello! I'm UhurU's AI assistant. To make our conversation a bit more personal, could you tell me your name? I'm ready to help you uplift your technological edge, optimize your tax planning, or uncover new avenues for investment.";
