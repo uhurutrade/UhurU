d28ed61af31b8f16987cd6bbf89f0a0f5fa537b0
Uhuru Trade <uhurutradeuk@gmail.com>
2025-07-18 18:03:46 +0000

    mas que ese hola como te llamas ?, algo asi , como podrias decirme tu no

--- src/chatbot/chatbot-welcome.ts
+++ src/chatbot/chatbot-welcome.ts
@@ -1,2 +1,2 @@
 
-export const chatbotWelcomeMessage = "Hello! I'm UhurU's AI assistant. To make our conversation a bit more personal, could you tell me your name? I'm ready to help you uplift your technological edge, optimize your tax planning, or uncover new avenues for investment.";
+export const chatbotWelcomeMessage = "Hello! I'm UhurU's AI assistant. To make our conversation a bit more personal, could you please tell me your name? I'm ready to help you uplift your technological edge, optimize your tax planning, or uncover new avenues for investment.";
