ecbf81ab837aa193002c4c1a0ef5e91bdf0c9fd2
Uhuru Trade <uhurutradeuk@gmail.com>
2025-10-18 08:44:06 +0000

    Release estable

--- src/chatbot/chatbot-welcome.ts
+++ src/chatbot/chatbot-welcome.ts
@@ -1,2 +1,1 @@
-
-export const chatbotWelcomeMessage = "Hello! I'm UhurU's AI assistant. To make our conversation a bit more personal, could you please tell me your name? I'm ready to help you uplift your technological edge, optimize your tax planning, or uncover new avenues for investment.";
+export const chatbotWelcomeMessage = "Welcome to UhurU !.I’m your multilingual AI assistant, ready to support you with technology optimization, strategic tax planning, and innovative investment solutions.";
