90d598e71772899770604680a562f87be1ae19b3
Uhuru Trade <uhurutradeuk@gmail.com>
2026-03-20 17:46:08 +0100

    .

--- src/components/uhuru/features.tsx
+++ src/components/uhuru/features.tsx
@@ -28,7 +28,7 @@ export default function Features() {
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl font-headline">Our Capabilities</h2>
             <p className="max-w-[900px] text-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-              Explore our wide range of expert services designed to elevate your business.
+              Explore our wide range of expert services designed to elevate your business
             </p>
           </div>
         </div>
