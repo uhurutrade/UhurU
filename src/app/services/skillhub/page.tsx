a76a40015281cce5ee69bc0b39f03fc9cab388ec
Uhuru Trade <uhurutradeuk@gmail.com>
2026-04-03 09:52:52 +0200

    .#	modificados:     src/app/services/skillhub/page.tsx

--- src/app/services/skillhub/page.tsx
+++ src/app/services/skillhub/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { registerUser, loginUser, requestPasswordReset } from '@/actions/auth';
 import { 
   User, Mail, Lock, Building, MapPin, Building2, Phone, Globe, Home, 
