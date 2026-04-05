9d73f803bb17e64a664059f79d1c2de9af5f162f
Uhuru Trade <uhurutradeuk@gmail.com>
2026-04-05 13:58:28 +0200

    UI: Raised the AI chat bubble position.

--- src/components/uhuru/ChatWidget.tsx
+++ src/components/uhuru/ChatWidget.tsx
@@ -140,7 +140,7 @@ const ChatWidgetContent = () => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-12 right-6 z-50">
        <TooltipProvider>
       {isOpen && (
           <div
